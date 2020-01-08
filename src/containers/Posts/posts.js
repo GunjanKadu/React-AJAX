e69f705166c3d3b0b3a58b722c77d57869e3083b
Gunjan Kadu <gunjankadu@Gunjans-MacBook-Air.local>
2020-01-08 22:56:00 +0100

    Preparing for routing and adjusting the code base

---
+++ src/containers/Posts/posts.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import axios from "../../axios";
+import Post from "../../components/Post/Post";
+import "./posts.css";
+
+class Posts extends Component {
+  state = {
+    posts: []
+  };
+  componentDidMount() {
+    axios
+      .get("/posts")
+      .then(response => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: "Gunjan"
+          };
+        });
+        this.setState({ posts: updatedPosts });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  postSelectHandler = id => {
+    this.setState({ selectedPostId: id });
+  };
+
+  render() {
+    let post = <p style={{ textAlign: "Center" }}>Something Went Wrong</p>;
+
+    if (!this.state.error) {
+      post = this.state.posts.map(post => {
+        return (
+          <Post
+            key={post.id}
+            title={post.title}
+            author={post.author}
+            click={() => this.postSelectHandler(post.id)}
+          />
+        );
+      });
+    }
+    return <section className="Posts">{post}</section>;
+  }
+}
+
+export default Posts;
